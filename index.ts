f0bda604ac4bf0415b5e1e1bb5366f71086c1cda
firesquid6 <jdeiss06@gmail.com>
2023-12-27 22:16:54 -0600

    use build folder

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import { Elysia } from "elysia";
-import { staticPlugin } from "@elysiajs/static";
+import { html } from "@elysiajs/html";
 
 new Elysia()
-  .use(
-    staticPlugin({
-      assets: "static",
-      prefix: "/",
-    }),
-  )
+  .use(html())
+  .get("*", ({ params }) => {
+    const route: string = params["*"];
+    const split = route.split(".");
+    if (route.split.length > 1 && split[split.length - 1] !== "html") {
+      const filepath = "./build/" + route;
+      console.log(filepath);
+      return Bun.file(filepath);
+    }
+
+    return `<h1>Hello: ${route}</h1>`;
+  })
   .listen(3000);
 
-console.log("Server running at http://localhost:8080");
+console.log("Server running at http://localhost:3000");
